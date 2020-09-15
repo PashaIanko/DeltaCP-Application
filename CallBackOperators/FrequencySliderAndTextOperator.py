b82e8c3b7edb33b5979ed6b4eadde571bb3ec531
Pavel Ianko <yanpavevg@yandex.ru>
2020-09-15 13:26:54 +0700

    FrequencySliderAndTextOperator began synch slider

--- CallBackOperators/FrequencySliderAndTextOperator.py
+++ CallBackOperators/FrequencySliderAndTextOperator.py
@@ -12,11 +12,32 @@ class FrequencySliderAndTextOperator(CallBackOperator):
         self.window = window
 
 
-        window.OutputFrequencylineEdit.setValidator(QDoubleValidator(
-            FrequencySettingGUIParameters.FrequencySliderMin,
-            FrequencySettingGUIParameters.FrequencySliderMax,
-            2))
+        #window.OutputFrequencylineEdit.setValidator(QDoubleValidator(
+        #    FrequencySettingGUIParameters.FrequencySliderMin,
+        #    FrequencySettingGUIParameters.FrequencySliderMax,
+        #    2))
+        # window.OutputFrequencylineEdit.setValidator(QDoubleValidator(0.0, 100.0, 2))
 
-        # window.OutputFrequencylineEdit.textChanged.connect(self.SetSlider)
+        window.OutputFrequencylineEdit.textEdited.connect(self.UpdateFrequencySlider)
+        window.FrequencySetSlider.valueChanged.connect(self.UpdateFrequencyLineEdit)
+
+    def UpdateFrequencySlider(self):
+        lineEditText = self.window.OutputFrequencylineEdit.text()
+
+        if(len(lineEditText) == 0):
+            lineEditText = '0'
+        try:
+            value = float(lineEditText) * 10.0
+            self.window.FrequencySetSlider.setValue(value)
+        except:
+            pass
+
+    def UpdateFrequencyLineEdit(self):
+        try:
+            self.window.OutputFrequencylineEdit.setText(str(self.window.FrequencySetSlider.value() / 10))
+        except:
+            print('caught!')
+
+
 
 
