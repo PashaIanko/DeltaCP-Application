89c6f81b3def6fd748f280cd29ee519e7b3360bf
Pavel Ianko <yanpavevg@yandex.ru>
2020-09-17 10:26:43 +0700

    Timer works in a new thread, does not block main

--- CallBackOperators/StartSendingOperator.py
+++ CallBackOperators/StartSendingOperator.py
@@ -1,8 +1,10 @@
 from CallBackOperator import CallBackOperator
 from SignalGenerationPackage.SignalData import SignalData
 from SignalSendingPackage.SignalTimer import SignalTimer
+from threading import Thread
 import time
 import sys
+
 
 class StartSendingOperator(CallBackOperator):
 
@@ -10,14 +12,15 @@ class StartSendingOperator(CallBackOperator):
         self.UserInterface = None  # TODO: Перенести в родительский класс UserInterface
         self.Timer = SignalTimer(interval=1.0, function=self.TestTimer)
         self.FunctionWasCalled = False
+        self.SendingThreadWasLaunched = False
+        self.SendingThread = None  # Поток, в котором отправляем данные сигнала
 
 
     def ConnectCallBack(self, UserInterface):
         self.UserInterface = UserInterface
         UserInterface.pushButtonStartSignalSending.clicked.connect(self.StartSendingSignal)
 
-
-    def StartSendingSignal(self):
+    def ThreadFunc(self):
         print(SignalData.x, SignalData.y)
         Signal = SignalData.x.copy()
         Time = SignalData.y.copy()
@@ -25,19 +28,30 @@ class StartSendingOperator(CallBackOperator):
         DeltaTimes = [1, 2, 3, 4, 5]  # [dt_next - dt_prev for dt_next, dt_prev in zip(range(1, N), range(0, N-1))]
 
         print(f' start: {time.asctime()}')
-        self.Timer.reset(DeltaTimes[0])
+        self.Timer.interval = DeltaTimes[0]
+        self.Timer.run()
         try:
             i = 0
             while True:
                 if self.FunctionWasCalled:
-                    i += 1
                     self.FunctionWasCalled = False
+                    i += 1
                     self.Timer.reset(DeltaTimes[i])
                 if i == len(DeltaTimes) - 1:
                     break
-            print('Finished Succeeded')
         except:
             print(sys.exc_info())
+
+
+    def StartSendingSignal(self):
+        if self.SendingThreadWasLaunched == False:  # Или поток завершился или не был запущен
+            self.SendingThreadWasLaunched = True
+            self.SendingThread = Thread(target=self.ThreadFunc)
+            self.SendingThread.start()  # TODO: Защита, если два раза нажали Start Sending - чтобы два раза поток не запускался
+        else:
+            pass
+
+
 
     def TestTimer(self):
         print('Test', time.asctime())
