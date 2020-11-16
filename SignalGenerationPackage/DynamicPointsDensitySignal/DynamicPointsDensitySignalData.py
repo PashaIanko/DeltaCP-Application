d2a648a6b5a508663a6934da07b68cb3ce3c718c
Pavel Ianko <yanpavevg@yandex.ru>
2020-11-16 14:22:28 +0700

    added dynamic pts density signal

--- SignalGenerationPackage/DynamicPointsDensitySignal/DynamicPointsDensitySignalData.py
+++ SignalGenerationPackage/DynamicPointsDensitySignal/DynamicPointsDensitySignalData.py
@@ -8,12 +8,15 @@ class DynamicPointsDensitySignalData(SignalData):
         self.VerticalOffset = 0
 
         self.StartTime = 0
-        self.AccelerationTime = 0.01  # Чтоб избежать деления на ноль
+        self.AccelerationTime = 0
         self.PlateauTime = 0
-        self.DecelerationTime = 0.01  # Чтоб избежать деления на ноль
+        self.DecelerationTime = 0
         self.EndTime = 0
 
         self.X_from = 0
-        self.PointsDensity = 0
         self.LowLevelFrequency = 0
         self.HighLevelFrequency = 0
+        self.PointsDensity = 0
+
+        self.CriticalAccelerationTangent = 1.6  # Hz per second
+        self.CriticalDecelerationTangent = -1.6  # Hz per second
