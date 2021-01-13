46d9a986f688d0cc46f3e2f3f819bb3b005e7a90
Pavel Ianko <yanpavevg@yandex.ru>
2021-01-13 15:38:05 +0700

    initial commit - in process of making abstract class

--- CallBackOperators/StartSendingOperator.py
+++ CallBackOperators/StartSendingOperator.py
@@ -1,68 +1,12 @@
-from CallBackOperator import CallBackOperator
 from SignalGenerationPackage.SignalData import SignalData
 from SignalSendingPackage.SignalTimer import SignalTimer
-from SignalSendingPackage.SignalVisualizer import SignalVisualizer
-from threading import Thread
-from DeltaCPClient import DeltaCPClient
-from time import sleep
-import sys
+from SignalSendingPackage.SignalSendingOperator import SignalSendingOperator
 
 
-class StartSendingOperator(CallBackOperator):
+class StartSendingOperator(SignalSendingOperator):
 
     def __init__(self):
-        self.UserInterface = None  # TODO: Перенести в родительский класс UserInterface
-        self.TimeStamp = 0
-        self.ValueToSend = 0
-        self.Timer = SignalTimer(interval=1.0, function=self.TestTimer)
-        self.DeltaCPClient = DeltaCPClient()
-        self.FunctionWasCalled = False
-        self.SendingThreadWasLaunched = False
-        self.SendingThread = None  # Поток, в котором отправляем данные сигнала
-
-        self.SignalVisualizer = None #SignalVisualizer()
-        self.SignalVisualizerConstructed = False
-
-        self.PointsIterator = 0  # Just Counter to iterate over [x, y] arrays of SignalData
-        self.SendingOnPause = False
-        self.SendingStopped = False
-        self.EndlessSendingEnabled = False
-        self.CycleFinishedSuccessfully = False
-        self.CommandExecutionTime = 0.23  # Часть времени уходит на исполнение команды (отправку частоты на
-                                        # частотник, обновление отрисовки). Надо подобрать этот параметр,
-                                        # и начинать исполнение команды на dt раньше, чтобы учесть задержку по времени
-                                        # на исполнение команды
-
-
-
-    def ConnectCallBack(self, UserInterface):
-        self.UserInterface = UserInterface
-        UserInterface.pushButtonStartSignalSending.clicked.connect(self.StartSendingSignal)
-        UserInterface.PauseSendingradioButton.toggled.connect(self.PauseSending)
-        UserInterface.ResumeSendingradioButton.toggled.connect(self.ResumeSending)
-        UserInterface.pushButtonStopSignalSending.clicked.connect(self.StopSendingSignal)
-        UserInterface.EndlessSendingcheckBox.stateChanged.connect(lambda: self.EnableEndlessSending())
-
-
-    def EnableEndlessSending(self):
-        self.EndlessSendingEnabled = \
-            self.UserInterface.EndlessSendingcheckBox.isChecked()
-
-    def PauseSending(self):
-        if self.UserInterface.PauseSendingradioButton.isChecked():
-            print('Paused')
-            self.SendingOnPause = True
-            self.UserInterface.ResumeSendingradioButton.setChecked(False)
-        else:
-            self.UserInterface.ResumeSendingradioButton.setChecked(True)
-
-    def ResumeSending(self):
-        if self.UserInterface.ResumeSendingradioButton.isChecked():
-            print('Resumed')
-            self.SendingOnPause = False
-            self.UserInterface.PauseSendingradioButton.setChecked(False)
-        else:
-            self.UserInterface.PauseSendingradioButton.setChecked(True)
+        super().__init__()
 
     def ExecuteSending(self, Time):
         DeltaTimes = SignalData.dx
@@ -73,7 +17,7 @@ class StartSendingOperator(CallBackOperator):
         self.TimeStamp = Time[self.PointsIterator]
         self.ValueToSend = SignalData.y[self.PointsIterator]
 
-        if self.Timer.if_started == True:  # Если уже дали старт таймеру на предудущем цикле
+        if self.Timer.if_started:  # Если уже дали старт таймеру на предудущем цикле
             self.Timer.reset(DeltaTimes[0] - self.CommandExecutionTime)
         else:
             self.Timer.interval = DeltaTimes[0] - self.CommandExecutionTime
@@ -128,9 +72,6 @@ class StartSendingOperator(CallBackOperator):
                 self.SignalVisualizer.Restart(Time)
                 self.ExecuteSending(Time)
 
-                #self.RestartSignalIterator()
-                #self.RestartVisualization(Time)
-                #self.ExecuteSending(Time)
 
     def Restart(self, Time):
         self.CycleFinishedSuccessfully = False
@@ -142,55 +83,11 @@ class StartSendingOperator(CallBackOperator):
         self.ExecuteSending(Time)
 
 
-    def RestartVisualization(self, TimeArray):
-        print(f'Restarting Visualization!!')
-        self.SignalVisualizer.Restart(TimeArray)
 
 
 
 
 
-
-    def LaunchSendingThread(self):
-        self.SendingThread = Thread(target=self.ThreadFunc)
-        self.SendingThread.start()
-
-
-
-    # TODO: Исправить баг, когда StopSignalSending, потом рестарт - не отрисовывается визуализация
-    def StartSendingSignal(self):
-        if self.SendingThread is None:
-            print(f'launching thread')
-            if not self.SignalVisualizerConstructed:
-                self.SignalVisualizer = SignalVisualizer()
-            self.DeltaCPClient.SendStart()
-            self.LaunchSendingThread()
-        else:
-            if not self.SendingThread.is_alive():
-                print(f'launching thread')
-                self.SignalVisualizer.Restart(TimeArray=[])
-                self.RestartSignalIterator()
-                self.SendingStopped = False  # Надо почистить этот флаг
-                self.LaunchSendingThread()
-            else:
-                print(f'Prev sending thread is executing, cant launch one')
-
-    def StopSendingSignal(self):
-        self.DeltaCPClient.SetFrequency(0.0)
-        self.DeltaCPClient.SendStop()
-        self.SendingStopped = True
-
-    def RestartSignalIterator(self):
-        self.PointsIterator = 0
-
-    def TestTimer(self):
-        value_to_send = int(self.ValueToSend * 100)  # Привести к инту, иначе pymodbus выдаёт ошибку
-        self.DeltaCPClient.SetFrequency(value_to_send)
-        CurrentFreq = self.DeltaCPClient.RequestCurrentFrequency()
-        self.SignalVisualizer.UpdateSetFrequency(self.TimeStamp, self.ValueToSend)
-        self.SignalVisualizer.UpdateCurrentFrequency(self.TimeStamp, CurrentFreq)
-
-        self.FunctionWasCalled = True
 
 
 
