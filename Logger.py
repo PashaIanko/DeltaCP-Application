91f1da90bbb35ddb5ed36baae4011cf31f760f32
Pavel Ianko <yanpavevg@yandex.ru>
2021-01-19 16:22:27 +0700

    implemented class Logger

---
+++ Logger.py
@@ -0,0 +1,26 @@
+import logging
+from Singleton import Singleton
+
+
+@Singleton
+class Logger:
+
+    def __init__(self, LoggerName, LogLevel, FileName=None):
+        self.Logger = logging.getLogger(LoggerName)
+        self.Logger.setLevel(LogLevel)
+
+        if FileName is not None:
+            self.Handler = logging.FileHandler(FileName + '.log')
+        else:
+            import sys
+            self.Handler = logging.StreamHandler(sys.stdout)
+
+        self.Formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.Handler.setFormatter(self.Formatter)
+        self.Logger.addHandler(self.Handler)
+
+    def Info(self, msg):
+        self.Logger.info(msg)
+
+    def Debug(self, msg):
+        self.Logger.debug(msg)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 from ApplicationManager import ApplicationManager
+from Logger import Logger
+import logging
 from PyQt5 import QtWidgets
 
 if __name__ == "__main__":
@@ -10,5 +11,8 @@ if __name__ == "__main__":
         MainWindow = ApplicationManager()
     except:
         print(sys.exc_info())
+
+    AppLogger = Logger('TestLogger', logging.INFO)
+    AppLogger.Info('Abc')
     MainWindow.show()
     sys.exit(app.exec_())
