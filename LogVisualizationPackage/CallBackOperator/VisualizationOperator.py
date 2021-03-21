bdb08c92d4a247c1c9ea6e0066896db8dd30d0e4
Pavel Ianko <yanpavevg@yandex.ru>
2021-03-21 18:25:28 +0700

    Added PopUpNotifier for wrond dir and abscent columns

--- LogVisualizationPackage/CallBackOperator/VisualizationOperator.py
+++ LogVisualizationPackage/CallBackOperator/VisualizationOperator.py
@@ -1,9 +1,16 @@
 from CallBackOperator import CallBackOperator
 import pandas as pd
+from PopUpNotifier.PopUpNotifier import PopUpNotifier
 
 class VisualizationOperator(CallBackOperator):
     def __init__(self):
         self.UserInterface = None
+        self.NecessaryColumns = [
+            'Expect Time',
+            'Expect Freq, Hz',
+            'Real Time (Synchronized), Sec',
+            'Real Freq, Hz'
+        ]  # Необходимые имена колонок
 
 
 
@@ -14,19 +21,31 @@ class VisualizationOperator(CallBackOperator):
     def VisualizeLog(self):
         logfile_dir = self.UserInterface.LogFilenamelineEdit.text() + '.xlsx'
         log_plot = self.UserInterface.LogPlot
+
         try:
-
-
             log_df = pd.read_excel(logfile_dir)
-            t_expect = log_df['Expect Time'].values
-            f_expect = log_df['Expect Freq, Hz'].values
-            t_real = log_df['Real Time (Synchronized), Sec'].values
-            f_real = log_df['Real Freq, Hz'].values
+            cols_correct = self.check_columns_correctness(log_df)
 
-            log_plot.plot(t_expect, f_expect)
-            #log_plot.plot(t_real, f_real)
+            if cols_correct:
+                t_expect = log_df['Expect Time'].values
+                f_expect = log_df['Expect Freq, Hz'].values
+                t_real = log_df['Real Time (Synchronized), Sec'].values
+                f_real = log_df['Real Freq, Hz'].values
 
+                log_plot.plot(t_expect, f_expect)
+                #log_plot.plot(t_real, f_real)
+
+        except FileNotFoundError:
+            PopUpNotifier.Error(f'Cannot find file {logfile_dir}')
         except:
             import sys
-            print(sys.exc_info())
+            print(sys.exc_info())  # TODO: wrong filename PopUpNotifier сделать
+
+    def check_columns_correctness(self, df):
+        cols = df.columns
+        for c in self.NecessaryColumns:
+            if not c in cols:
+                PopUpNotifier.Error(f'The .xlsx file does not have column "{c}".\nIncorrect source data!')
+                return False
+        return True
 
