a9d8a0da8089f1126fc3e3cf3fdb32865676814d
Pavel Ianko <yanpavevg@yandex.ru>
2021-04-19 13:05:11 +0700

    fixed bug for slider editing (HighLevel Frequency)

--- Ranges.py
+++ Ranges.py
@@ -20,6 +20,6 @@ class EdgeSignalRanges:
     t_plateau_range = ValueRange(min=0, max=80, decimals=2)
     t_deceleration_range = ValueRange(min=0, max=80, decimals=2)
     t_end_range = ValueRange(min=0, max=50, decimals=2)
-    high_freq_range = ValueRange(min=1, max=50, decimals=2)
+    high_freq_range = ValueRange(min=0, max=50, decimals=2)
     low_freq_range = ValueRange(min=0, max=49, decimals=2)
     request_freq_range = ValueRange(min=0.3, max=1.0, decimals=2)
