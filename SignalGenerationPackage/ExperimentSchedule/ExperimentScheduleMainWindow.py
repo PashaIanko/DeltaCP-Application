29c5b50f921fcd9992138a58247095811a32165a
Pavel Ianko <yanpavevg@yandex.ru>
2021-05-06 12:54:21 +0700

    Implemented ExperimentScheduleMainWindow

--- SignalGenerationPackage/ExperimentSchedule/ExperimentScheduleMainWindow.py
+++ SignalGenerationPackage/ExperimentSchedule/ExperimentScheduleMainWindow.py
@@ -12,3 +12,39 @@ class ExperimentScheduleMainWindow(SignalMainWindow):
     # overridden
     def init_plot(self):
         self.plot = self.user_interface.plot_widget
+
+    # overridden
+    def get_start_button(self):
+        return self.user_interface.pushButtonStartSignalSending
+
+    # overridden
+    def get_pause_radio_button(self):
+        return self.user_interface.PauseSendingradioButton
+
+    # overridden
+    def get_resume_radio_button(self):
+        return self.user_interface.ResumeSendingradioButton
+
+    # overridden
+    def get_stop_button(self):
+        return self.user_interface.pushButtonStopSignalSending
+
+    # overridden
+    def get_endless_send_radiobutton(self):
+        return self.user_interface.EndlessSendingradioButton
+
+    # overridden
+    def get_cycle_send_radiobutton(self):
+        return self.user_interface.SendCyclesradioButton
+
+    # overridden
+    def get_cycles_number_widget(self):
+        return self.user_interface.CyclesNumberspinBox
+
+    # overridden
+    def get_LCD_display(self):
+        return self.user_interface.lcdNumber
+
+    # overridden
+    def get_log_filename_lineedit(self):
+        return self.user_interface.LogFilenamelineEdit
