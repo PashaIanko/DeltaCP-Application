58614e0fcac1c2cef7d7ffb6b95bb5f69ea5ff61
Pavel Ianko <yanpavevg@yandex.ru>
2021-05-09 10:47:12 +0700

    Now ScheduleAutoFill reads values from excel

--- SignalGenerationPackage/ExperimentSchedule/CallBacks/ScheduleAutoFillOperator.py
+++ SignalGenerationPackage/ExperimentSchedule/CallBacks/ScheduleAutoFillOperator.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from PopUpNotifier.PopUpNotifier import PopUpNotifier
+
 
 class ScheduleAutoFillOperator:
     def __init__(self, window, param_names, model, configs_folder):
@@ -12,6 +14,34 @@ class ScheduleAutoFillOperator:
     def ConnectCallBack(self):
         self.window.AutoFillpushButton.clicked.connect(self.AutoFill)
 
+    def check_data_validity(self, configs_df):
+        column_names = configs_df.columns
+        absent_columns = []
+        for p in self.param_names:
+            if not (p in column_names):
+                absent_columns.append(p)
+        return absent_columns
+
+
     def AutoFill(self):
-        config_filepath = self.configs_folder + self.config_line_edit.text() + '.xlsx'
-        configs_data = pd.read_excel(config_filepath)
+        config_name = self.config_line_edit.text()
+        config_filepath = self.configs_folder + config_name + '.xlsx'
+        configs_df = pd.read_excel(config_filepath)
+
+        absent_columns = self.check_data_validity(configs_df)
+        if len(absent_columns) == 0:
+            # Значит, все необходимые колонки
+            # были заполнены, данные правильные
+            frequencies = configs_df['Frequencies'].values
+            seconds = configs_df['Seconds'].values
+            request_freq = configs_df['Request every N seconds'].values[0]
+
+
+
+        else:
+            PopUpNotifier.Error(
+                f'Missing Columns in file "{config_name}":\n{absent_columns}'
+            )
+
+        # Закроем файл
+        del self.configs_df
